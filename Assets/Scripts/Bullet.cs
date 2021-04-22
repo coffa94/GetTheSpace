45fffb23755d6bb46bca0055390d05ff1aae9892
Davide Coffaro <davidecoffaro@gmail.com>
2021-04-22 19:12:59 +0200

    MS2_bullet and bulletEnemy management
    - prefab bullet1 and bullet2 modified with rigidbody2D
    - prefab bulletEnemy1 modified with rigidbody2D
    - script bullet added component and movement with rigidbody2D, removed logic to destroy the bullet when exit the screen, OnTriggerEnter when hit its enemiesTarget destroying itself and the enemy hit
    - script bulletEnemy component rigidbody2D to move the bullet towards the direction of the destination set using the speedBullet, OnTriggerEneter when hit the playerTarget destroying itself

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,36 +9,31 @@ public class Bullet : MonoBehaviour
     private int _playerNumber;
     private int _shotDirection;
 
+    private Rigidbody2D _rigidbody;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //destroy player1's bullet when they exit the top screen
-        if (_playerNumber==1 && transform.position.y > 12f) {
-            Destroy(gameObject);
-            return;
-        }
+        
+    }
 
-        //destroy player2's bullet when they exit the bottom screen
-        if (_playerNumber == 2 && transform.position.y < -12f) {
-            Destroy(gameObject);
-            return;
-        }
-
-
-
-        transform.Translate(new Vector3(0f, _shotDirection*speedBullet*Time.deltaTime, 0f));
+    private void FixedUpdate() {
+        //move the bullet in the scene
+        _rigidbody.MovePosition(new Vector2(_rigidbody.position.x, _rigidbody.position.y + _shotDirection * speedBullet));
     }
 
     public void SetPlayer(int playerNumber) {
         _playerNumber = playerNumber;
         Debug.Log("bullet " + _playerNumber + " shooted");
+
+        //set the direction of the bullet
         if (_playerNumber == 1) {
             _shotDirection = 1;
         } else {
@@ -46,5 +41,16 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponent<EnemyController>()) {
+            //TODO controllare se nemico è stato colpito dal proiettile del suo giocatore bersaglio
+            if (collision.GetComponent<EnemyController>().GetPlayerTarget() == _playerNumber) {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+            Debug.Log("Nemico " + _playerNumber + " colpito");
+        }
+    }
+
+
 }
--- Assets/Scripts/BulletEnemy.cs
+++ Assets/Scripts/BulletEnemy.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class BulletEnemy : MonoBehaviour
 {
-    [SerializeField] private float speedMovement;
+    [SerializeField] private float speedBullet=0.1f;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _destination;
+    private Vector2 _direction;
+    private int _playerTarget;
+
+    public Vector2 Destination { get => _destination; set => _destination = value; }
+    public int PlayerTarget { get => _playerTarget; set => _playerTarget = value; }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        //set direction of the bullet so it moves towards the destination set, moving out of the screen if the player is not hit
+        _direction = (Destination - _rigidbody.position)*1000f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate( 0f, speedMovement, 0f);
+        _rigidbody.MovePosition(Vector2.MoveTowards(_rigidbody.position, _direction, speedBullet)); 
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponent<PlayerController>()) {
+            if (PlayerTarget == collision.GetComponent<PlayerController>().PlayerNumber) {
+                Debug.Log("Giocatore " + collision.GetComponent<PlayerController>().PlayerNumber + " colpito dal proiettilo nemico " + PlayerTarget);
+                //destroy the bullet after its collision with the playerTarget of the enemy bullet
+                Destroy(gameObject);
+            }
+            
+        }
     }
 }
